741272e7f1ff1963e51e6ebf77d92445ea2549c2
Nor014 <metal_14@mail.ru>
2020-01-16 23:28:43 +0700

    fix

--- src/Frontend/Components/MainPage/Welcome/Welcome.jsx
+++ src/Frontend/Components/MainPage/Welcome/Welcome.jsx
@@ -8,7 +8,8 @@ export default class Welcome extends React.Component {
     return (
       <div className="welcome">
         <div className="welcome__inner container">
-          <h1 className='welcome__title text text_level_first text_theme_white'><span className='text text_weight_200'>Вся жизнь -</span> путешествие!</h1>
+          <h1 className='welcome__title text text_level_first text_theme_white'>
+            <span className='text text_weight_200'>Вся жизнь -</span> путешествие!</h1>
           <FindTickets fromComponent='Welcome' />
         </div>
       </div>
